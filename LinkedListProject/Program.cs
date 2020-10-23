1caa820943eb82eaea753b941b2b26161b1d8109
BlaKe00786 <sameershaikh403@gmail.com>
2020-10-23 20:48:38 +0530

    [Sameer]Refactor: added nodes according to usecase.

--- LinkedListProject/Program.cs
+++ LinkedListProject/Program.cs
@@ -7,9 +7,9 @@ namespace LinkedListProject
         static void Main(string[] args)
         {
             LinkedList list = new LinkedList(); 
-            list.Add(56);
-            list.Add(30);
             list.Add(70);
+            list.Add(30);
+            list.Add(56);
             list.Display();
         }
     }
